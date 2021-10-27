cc1cbff05c50955e07d2d90164375c8d5a7af220
Sergei Shubenkov <66917877+SergunWE@users.noreply.github.com>
2021-10-27 17:31:13 +0300

    Разделение классов для сохранения в отдельные файлы

---
+++ Assets/Objects/Timer/Scripts/SaveObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] 
+public static class SaveObject
+{
+    private static List<TimerData> allTimers = new List<TimerData>();
+    public static List<TimerData> SaveTimers()
+    {
+        TimerManager tm = GameObject.FindObjectOfType<TimerManager>();
+        for (var i = 0; i < tm.GetNumberTimers(); i++)
+        {
+            var timer = tm.GetTimer(i);
+            TimerData td = new TimerData(timer);
+            allTimers.Add(td);
+        }
+        Debug.Log("Файл сохранён в " + Application.persistentDataPath);
+        return allTimers;
+    }
+    
+    [System.Serializable] 
+    public struct TimerData
+    {
+        float timerTime;
+        string action;
+        bool isActived;
+        public string name;
+
+        public TimerData(float t, string a, bool isA, string n)
+        {
+            timerTime = t;
+            action = a;
+            isActived = isA;
+            name = n;
+        }
+
+        public TimerData(Timer timer)
+        {
+            timerTime = timer.GetTimeTimer;
+            action = timer.GetAction;
+            isActived = timer.GetActive;
+            name = timer.GetName;
+        }
+        
+    }
+}
--- Assets/Objects/Timer/Scripts/TimerSaveLoad.cs
+++ Assets/Objects/Timer/Scripts/TimerSaveLoad.cs
@@ -5,70 +5,25 @@ using UnityEngine;
 
 public static class SaveLoadTimer
 {
+    private const string saveNameFile = "/savedTimers.st";
     public static List<SaveObject.TimerData> savedTimers = new List<SaveObject.TimerData>();
 
     public static void Save()
     {
         savedTimers = SaveObject.SaveTimers();
         BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create (Application.persistentDataPath + "/savedTimers.st");
+        FileStream file = File.Create (Application.persistentDataPath + saveNameFile);
         bf.Serialize(file, savedTimers);
         file.Close();
     }
 
     public static void Load()
     {
-        if(File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+        if(File.Exists(Application.persistentDataPath + saveNameFile)) {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
-            SaveLoadTimer.savedTimers = (List<SaveObject.TimerData>)bf.Deserialize(file);
+            FileStream file = File.Open(Application.persistentDataPath + saveNameFile, FileMode.Open);
+            savedTimers = (List<SaveObject.TimerData>)bf.Deserialize(file);
             file.Close();
         }
-    }
-    
-}
-
-[System.Serializable] 
-public static class SaveObject
-{
-    private static List<TimerData> allTimers = new List<TimerData>();
-
-    public static List<TimerData> SaveTimers()
-    {
-        TimerManager tm = GameObject.FindObjectOfType<TimerManager>();
-        for (var i = 0; i < tm.GetNumberTimers(); i++)
-        {
-            var timer = tm.GetTimer(i);
-            TimerData td = new TimerData(timer);
-            allTimers.Add(td);
-        }
-        Debug.Log("Файл сохранён в " + Application.persistentDataPath);
-        return allTimers;
-    }
-
-    [System.Serializable] 
-    public struct TimerData
-    {
-        float timerTime;
-        string action;
-        bool isActived;
-        public string name;
-
-        public TimerData(float t, string a, bool isA, string n)
-        {
-            timerTime = t;
-            action = a;
-            isActived = isA;
-            name = n;
-        }
-
-        public TimerData(Timer timer)
-        {
-            timerTime = timer.GetTimeTimer;
-            action = timer.GetAction;
-            isActived = timer.GetActive;
-            name = timer.GetName;
-        }
-        
     }
 }

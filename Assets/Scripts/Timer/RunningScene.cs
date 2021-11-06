9cc7c449380ce59a738453dc0f2bfb41816f89eb
Sergei Shubenkov <66917877+SergunWE@users.noreply.github.com>
2021-11-06 16:30:42 +0300

    Отображение таймера и его связка с сундуком

--- Assets/Scripts/Timer/RunningScene.cs
+++ Assets/Scripts/Timer/RunningScene.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 
 public class RunningScene: MonoBehaviour
 {
+    public Chest _chest;
     void Start()
     {
-        TimerManager.LoadTimersInGame();
-        //TimerManager.CreateTimer(new TimeSpan(0,0,30), "First");
+        //TimerManager.LoadTimersInGame();
+        TimerManager.CreateTimer(new TimeSpan(0,0,10), "First");
+        _chest.SetTimer(TimerManager.GetTimer(0));
         //TimerManager.CreateTimer(new TimeSpan(0,0,1), "Second");
         //SaveLoadTimer.Save();
     }

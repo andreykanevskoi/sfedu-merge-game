58e2a51daf3c8662e45afcb9d9f15769da6c9163
Sergei Shubenkov <66917877+SergunWE@users.noreply.github.com>
2021-11-20 22:41:18 +0300

    Pause

---
+++ Assets/Scripts/Pause/QuitButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitButton : MonoBehaviour
+{
+    public void AutoQuit()
+    {
+        if (SceneManager.GetActiveScene().handle == 0)
+        {
+            Application.Quit();
+        }
+
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitInIndex(int sceneIndex)
+    {
+        if (SceneManager.sceneCount <= sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+    }
+
+    public void QuitInName(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}

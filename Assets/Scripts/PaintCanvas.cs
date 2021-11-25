ed37415e3b887e9df876cb555fb7e26b0ec16d2f
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2021-11-25 12:20:54 +0300

    Перенос мини-игры в канвас

--- Assets/Scripts/PaintCanvas.cs
+++ Assets/Scripts/PaintCanvas.cs
@@ -1,35 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Paint : MonoBehaviour
-{
-
-    [Range(2, 512)]
+public class PaintCanvas : MonoBehaviour{
     [SerializeField] private int _textureSize = 128;
     [SerializeField] private TextureWrapMode _textureWrapMode;
     [SerializeField] private FilterMode _filterMode;
     [SerializeField] private Texture2D _texture;
     Texture2D _tmpT;
 
-    [SerializeField] private Camera _camera;
-    [SerializeField] private Collider _collider;
-
     [Range(15, 100)]
     [SerializeField] private int _brushSize = 30;
 
     private float _counter;
     private float _fullDirtyImg;
-    private int _oldRayX, _oldRayY;
+    private int _oldX, _oldY;
 
-    void OnValidate()
-    {
-        if (_texture == null)
-        {
+    private RectTransform _rectTransform;
+
+    private void Awake() {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    void OnValidate() {
+        if (_texture == null) {
             _texture = new Texture2D(_textureSize, _textureSize, TextureFormat.ARGB32, false);
         }
-        if (_texture.width != _textureSize)
-        {
+        if (_texture.width != _textureSize) {
             _texture.Resize(_textureSize, _textureSize);
         }
         _texture.wrapMode = _textureWrapMode;
@@ -41,13 +39,10 @@ public class Paint : MonoBehaviour
 
         _counter = _texture.width * _texture.height;// подсчет изначального размера грязной текстуры
         float fullImg = _counter;
-        for (int x = 0; x < _texture.width; x++)
-        {
-            for (int y = 0; y < _texture.height; y++)
-            {
+        for (int x = 0; x < _texture.width; x++) {
+            for (int y = 0; y < _texture.height; y++) {
                 Color color = _texture.GetPixel(x, y);
-                if (color.a == 0)
-                {
+                if (color.a == 0) {
                     _counter--;
                 }
             }
@@ -55,45 +50,55 @@ public class Paint : MonoBehaviour
         _fullDirtyImg = _counter;
     }
 
+    private bool InsideImage(Vector2 position) {
+        return RectTransformUtility.RectangleContainsScreenPoint(_rectTransform, position);
+    }
 
-    private void Update()
-    {
+    private void Update() {
         //_brushSize += (int)Input.mouseScrollDelta.y;
         if (Input.GetMouseButton(0))// очистка
         {
-            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (_collider.Raycast(ray, out hit, 100f))
-            {
-                int rayX = (int)(hit.textureCoord.x * _textureSize);
-                int rayY = (int)(hit.textureCoord.y * _textureSize);
+            Vector2 position = Input.mousePosition;
 
-                _texture.SetPixel(rayX, rayY, new Color(0, 0, 0, 0));
+            if (InsideImage(position)) {
+                Vector2 localPoint = new Vector2();
+                if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform, position, null, out localPoint)) {
+                    Debug.Log("Inside!");
+                    Debug.Log(localPoint);
 
-                if (_oldRayX != rayX || _oldRayY != rayY)
-                {
-                    DrawCircle(rayX, rayY);
-                    _oldRayX = rayX;
-                    _oldRayY = rayY;
+                    Vector2 uv = new Vector2(
+                        (localPoint.x + _rectTransform.rect.width * 0.5f) / _rectTransform.rect.width,
+                        (localPoint.y + _rectTransform.rect.height * 0.5f) / _rectTransform.rect.height
+                    );
+
+                    Debug.Log(uv);
+
+                    int newX = (int)(uv.x * _textureSize);
+                    int newY = (int)(uv.y * _textureSize);
+
+                    _texture.SetPixel(newX, newY, new Color(0, 0, 0, 0));
+
+                    if (_oldX != newX || _oldY != newY) {
+                        DrawCircle(newX, newY);
+                        _oldX = newX;
+                        _oldY = newY;
+                    }
+                    _texture.Apply();
                 }
-                _texture.Apply();
             }
         }
 
-      
+
         if (Input.GetKeyUp(KeyCode.Mouse0))// подсчет % грязи по отпусканию ЛКМ
         {
             _counter = _texture.width * _texture.height;
             float fullImg = _counter;
             float dirtyImg;
 
-            for(int x = 0; x < _texture.width; x++)
-            {
-                for (int y = 0; y < _texture.height; y++)
-                {
+            for (int x = 0; x < _texture.width; x++) {
+                for (int y = 0; y < _texture.height; y++) {
                     Color color = _texture.GetPixel(x, y);
-                    if (color.a == 0)
-                    {
+                    if (color.a == 0) {
                         _counter--;
                     }
                 }
@@ -110,34 +115,29 @@ public class Paint : MonoBehaviour
         }
     }
 
-    void OnApplicationQuit()// восстанавливает текстуру при завершении
-    {
+    // восстанавливает текстуру при завершении
+    void OnApplicationQuit() {
         Debug.Log("Application ending");
         Graphics.CopyTexture(_tmpT, _texture);
     }
 
     void DrawCircle(int rayX, int rayY)// круглая кисть
     {
-        for (int y = 0; y < _brushSize; y++)
-        {
-            for (int x = 0; x < _brushSize; x++)
-            {
+        for (int y = 0; y < _brushSize; y++) {
+            for (int x = 0; x < _brushSize; x++) {
                 float x2 = Mathf.Pow(x - _brushSize / 2, 2);
                 float y2 = Mathf.Pow(y - _brushSize / 2, 2);
                 float r2 = Mathf.Pow(_brushSize / 2 - 0.5f, 2);
 
-                if (x2 + y2 < r2)
-                {
+                if (x2 + y2 < r2) {
                     int pixelX = rayX + x - _brushSize / 2;
                     int pixelY = rayY + y - _brushSize / 2;
 
-                    if (pixelX >= 0 && pixelX < _textureSize && pixelY >= 0 && pixelY < _textureSize)
-                    {                    
+                    if (pixelX >= 0 && pixelX < _textureSize && pixelY >= 0 && pixelY < _textureSize) {
                         _texture.SetPixel(pixelX, pixelY, new Color(0, 0, 0, 0));
                     }
                 }
             }
         }
     }
-
 }

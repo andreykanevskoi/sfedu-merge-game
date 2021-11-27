94e04c75985c17b7111addcee0d3d4dbadd8082f
Sergei Shubenkov <66917877+SergunWE@users.noreply.github.com>
2021-11-27 18:25:48 +0300

    Сохранение состояния кнопок при нажатии кнопки выхода

--- Assets/Scripts/Save/ExitSaving.cs
+++ Assets/Scripts/Save/ExitSaving.cs
@@ -4,10 +4,18 @@ public class ExitSaving: MonoBehaviour
 {
     private void OnApplicationFocus(bool hasFocus)
     {
-        
+        if (!hasFocus)
+        {
+            Save();
+        }
     }
 
     private void OnApplicationQuit()
+    {
+        Save();
+    }
+
+    public void Save()
     {
         SoundManager.SaveSoundSettings();
     }

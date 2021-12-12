380b8b19a3380b90ca435e7a2cd9ab5ee1ee4f56
Sergun WE <sergunwe152@gmail.com>
2021-12-12 19:27:50 +0300

    Фикс багов, замеченные тестерами

---
+++ Assets/Scripts/Pause/PauseButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseButton : MonoBehaviour
+{
+    private void OnDisable()
+    {
+        GameEvents.current.TriggerPlayerInputDisable();
+    }
+
+    private void OnEnable()
+    {
+        GameEvents.current.TriggerPlayerInputEnable();
+    }
+}

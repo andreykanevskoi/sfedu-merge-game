37f16b286c2758500618c571985177350665d7db
KonovalovNikolai <64379069+KonovalovNikolai@users.noreply.github.com>
2021-12-15 11:44:13 +0300

    Иконка лопаты при изменения режима

---
+++ Assets/Scripts/UI/SwitchModeButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchModeButton : MonoBehaviour {
+
+    [SerializeField] private Sprite _handSprite;
+    [SerializeField] private Sprite _shovelSprite;
+    [SerializeField] private Button _button;
+
+    public void ChangeSprite() {
+        if (_button.image.sprite == _handSprite) {
+            _button.image.sprite = _shovelSprite;
+            return;
+        }
+        _button.image.sprite = _handSprite;
+    }
+}
